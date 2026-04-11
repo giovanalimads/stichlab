297a6073d98eee6297d96348d66a763a9edb4605
Giovana <gilimaolliveira@gmail.com>
2026-04-11 14:32:49 -0300

    commid

--- src/pages/EmptyHome.tsx
+++ src/pages/EmptyHome.tsx
@@ -1,8 +1,25 @@
+import "../index.css";
+
 function EmptyHome() {
   return (
     <div>
-      <h1>Bem-vindo 👋</h1>
-      <p>Você ainda não tem projetos.</p>
+      {/* logo */}
+      <div>
+        <h1 className="logo" class="bg-blue-500 p-4 rounded">
+          Stichlab
+        </h1>
+      </div>
+      {/* dash sem projetos */}
+      <div>
+        <h2>Seus projetos</h2>
+        <div>
+          <p>
+            Você ainda não criou nenhum gráfico
+            <br />
+            Crie seu primeiro projeto e comece a bordar!
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
